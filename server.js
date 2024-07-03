962804da5a9998d530d4fd09976cbe2a2b991496
Partha-2000-Snp <Parthasarathi.P@snp.com>
2024-07-03 19:57:35 +0530

    created login page in react

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = 80;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    if (username === 'partha' && password === '123') {
+        res.send('<script>alert("Hello"); window.location.href="/";</script>');
+    } else {
+        res.send('<script>alert("Try again"); window.location.href="/";</script>');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
